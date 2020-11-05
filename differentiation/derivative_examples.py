c16fb4043060d057fb927ba706a32f7a346ad442
Matthew Zettergren <mattzett@gmail.com>
2020-11-05 09:51:22 -0500

    adding a test of curl(grad())

--- differentiation/derivative_examples.py
+++ differentiation/derivative_examples.py
@@ -3,7 +3,7 @@
 """
 Created on Thu Oct 22 07:39:12 2020
 
-Illustrating various approaches to computing ordinaary derivatives
+Illustrating various approaches to computing ordinary derivatives
 
 @author: zettergm
 """
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 # Test function and exact derivative
 lx=20       #number of points in numerical grid
 x=np.linspace(-10,10,lx)
-dx=x[1]-x[0]
+dx=x[1]-x[0]     # grid step size or resolution
 y=np.sin(0.5*x)
 yprime=0.5*np.cos(0.5*x)
+ydprime=-0.25*np.sin(0.5*x)
 
 # Plots
 plt.figure(1)
@@ -26,9 +27,9 @@ plt.plot(x,yprime)
 plt.xlabel("x")
 plt.ylabel("y and dy/dx")
 
-# Centered difference approximation
+# Centered difference approximation to the first derivative (recommended approach for most problems)
 dydx=np.zeros(lx)
-dydx[0]=(y[1]-y[0])/dx                  #foward difference at the beginning
+dydx[0]=(y[1]-y[0])/dx                  #cannot do centered difference on the edges; foward difference at the beginning
 for ix in range(1,lx-1):
     dydx[ix]=(y[ix+1]-y[ix-1])/2/dx     #centered difference in the interior points
 dydx[lx-1]=(y[lx-1]-y[lx-2])/dx         #backward difference on the end
@@ -43,5 +44,15 @@ for ix in range(1,lx-1):
 dydx_fwd[lx-1]=(y[lx-1]-y[lx-2])/dx         #backward difference on the end
 
 plt.plot(x,dydx_fwd,'.')
-plt.legend(("y","dy/dx","centered","forward"))
+
+# Demo of second derivative, centered formula
+d2ydx2=np.zeros(lx)     # note that we are not computing edges values for 2nd deriv.
+for ix in range(1,lx-1):
+    d2ydx2[ix]=(y[ix+1]-2*y[ix]+y[ix-1])/dx**2
+
+# Plot second derivative and legend
+plt.plot(x,ydprime,'-')
+plt.plot(x,d2ydx2,'^')
+plt.legend(("y","dy/dx","centered","forward","2nd deriv. by hand","2nd deriv."))
 plt.show()
+
--- differentiation/partial_derivative_examples.py
+++ differentiation/partial_derivative_examples.py
@@ -20,14 +20,14 @@ x=np.linspace(-5,5,lx)
 y=np.linspace(-5,5,ly)
 [X,Y]=np.meshgrid(x,y)
 f=np.exp(-X**2/2/2)*np.exp(-Y**2/2/1)
-
+# Possibly compute gradient and laplacian analytically to test...
 
 # Coordinate differences
 dx=x[1]-x[0]
 dy=y[1]-y[0]
 
 
-# Compute the gradient of f
+# Compute the gradient of f, centered diffs. with fwd/bwd at edges
 gradx=np.zeros((ly,lx))       #note first index is treated as "y"
 grady=np.zeros((ly,lx))
 gradx[:,0]=(f[:,1]-f[:,0])/dx
@@ -41,7 +41,7 @@ for iy in range(1,lx-1):
 grady[lx-1,:]=(f[ly-1,:]-f[ly-2])/dy
 
 
-# Compute Laplacian f by taking div(grad(f))
+# Compute Laplacian f by taking div(grad(f)), centered diffs + edges as above
 divx=np.zeros((ly,lx))
 divx[:,0]=(gradx[:,1]-gradx[:,0])/dx
 for ix in range(0,lx-1):
@@ -54,6 +54,22 @@ for iy in range(0,ly-1):
     divy[iy,:]=(grady[iy+1,:]-grady[iy-1,:])/2/dy
 divy[ly-1,:]=(grady[ly-1,:]-grady[iy-2,:])/dy
 laplacianf=divx+divy
+
+
+# Take curl of the gradient (to check that is ~0); only concerned with z-comp
+# dvy/dx - dvx/dy is z-comp of curl
+curlx=np.zeros((ly,lx))
+curlx[:,0]=(grady[:,1]-grady[:,0])/dx
+for ix in range(0,lx-1):
+    curlx[:,ix]=(grady[:,ix+1]-grady[:,ix-1])/2/dx
+curlx[:,lx-1]=(grady[:,lx-1]-grady[:,lx-2])/dx
+
+curly=np.zeros((ly,lx))
+curly[0,:]=(gradx[1,:]-gradx[0,:])/dy
+for iy in range(0,ly-1):
+    curly[iy,:]=(gradx[iy+1,:]-gradx[iy-1,:])/2/dy
+curly[ly-1,:]=(gradx[ly-1,:]-gradx[ly-2,:])/dy
+curl=curlx-curly
 
 
 # Plot the function and derivatives
@@ -71,5 +87,13 @@ plt.pcolor(X,Y,laplacianf)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("laplacian(f)")
+plt.colorbar()
+plt.show()
+
+plt.figure(3)
+plt.pcolor(X[1:-2,1:-2],Y[1:-2,1:-2],curl[1:-2,1:-2])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("curl(grad(f))")
 plt.colorbar()
 plt.show()
